39369e4534c72dad20304fce7087ea967777bf2f
garethhallberg <gareth@ghallberg.co.uk>
2022-11-15 19:02:08 +0000

    App layout

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar as ExpoStatusBar} from 'expo-status-bar';
+import { StatusBar, SafeAreaView, StyleSheet, Text, View, Platform } from 'react-native';
+import { SearchView } from './src/components/SearchView'
+import { RestaurantListView } from './src/components/RestaurantListView'
+
+console.log(StatusBar.currentHeight);
 
 export default function App() {
+  const isAndroid = Platform.OS === 'android';
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+    <SafeAreaView style={{ flex: 1, marginTop: StatusBar.currentHeight }}>
+      <View style={styles.searchViewContainer}>
+        <Text style={styles.textStyle}>searching</Text>
+      </View>
+      <View style={styles.restaurantContainer}>
+        <Text style={styles.textStyle}>lists</Text>
+      </View>
+    </SafeAreaView>
+    <ExpoStatusBar style="auto" />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1, 
+    padding: 10,
+    backgroundColor: 'white',
   },
+  restaurantContainer: {
+    flex:1, 
+    backgroundColor: 'blue',
+    borderColor: 'darkblue',
+    borderWidth: 2,
+    padding: 10,
+  },
+  searchViewContainer: {
+    padding: 10,
+    backgroundColor: 'green',
+  },
+  textStyle: {
+      // top: 5,
+      left: 10,
+  }
 });
+
+
+
