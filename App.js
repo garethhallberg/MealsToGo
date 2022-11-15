decb91428b7050f3e8eef4f7c42d38eadf2f141a
garethhallberg <gareth@ghallberg.co.uk>
2022-11-15 19:47:25 +0000

    go lint go and be prettier

--- App.js
+++ App.js
@@ -1,49 +1,48 @@
-import { StatusBar as ExpoStatusBar} from 'expo-status-bar';
-import { StatusBar, SafeAreaView, StyleSheet, Text, View, Platform } from 'react-native';
-import { SearchView } from './src/components/SearchView'
-import { RestaurantListView } from './src/components/RestaurantListView'
+import React from "react";
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import { StatusBar, SafeAreaView, StyleSheet, Text, View } from "react-native";
 
 console.log(StatusBar.currentHeight);
 
 export default function App() {
-  const isAndroid = Platform.OS === 'android';
   return (
     <>
-    <SafeAreaView style={{ flex: 1, marginTop: StatusBar.currentHeight }}>
-      <View style={styles.searchViewContainer}>
-        <Text style={styles.textStyle}>searching</Text>
-      </View>
-      <View style={styles.restaurantContainer}>
-        <Text style={styles.textStyle}>lists</Text>
-      </View>
-    </SafeAreaView>
-    <ExpoStatusBar style="auto" />
+      <SafeAreaView style={styles.safeArea}>
+        <View style={styles.searchViewContainer}>
+          <Text style={styles.textStyle}>searching</Text>
+        </View>
+        <View style={styles.restaurantContainer}>
+          <Text style={styles.textStyle}>lists</Text>
+        </View>
+      </SafeAreaView>
+      <ExpoStatusBar style="auto" />
     </>
   );
 }
 
 const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight,
+  },
   container: {
-    flex: 1, 
+    flex: 1,
     padding: 10,
-    backgroundColor: 'white',
+    backgroundColor: "white",
   },
   restaurantContainer: {
-    flex:1, 
-    backgroundColor: 'blue',
-    borderColor: 'darkblue',
+    flex: 1,
+    backgroundColor: "blue",
+    borderColor: "darkblue",
     borderWidth: 2,
     padding: 10,
   },
   searchViewContainer: {
     padding: 10,
-    backgroundColor: 'green',
+    backgroundColor: "green",
   },
   textStyle: {
-      // top: 5,
-      left: 10,
-  }
+    // top: 5,
+    left: 10,
+  },
 });
-
-
-
