a492e10da45fcbb46a85ab63200a0eb66302b2fc
garethhallberg <gareth@ghallberg.co.uk>
2022-11-23 18:45:26 +0000

    fixup! Spacer for spacing

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -1,59 +1,27 @@
 import React from "react";
-import { View, Text } from "react-native";
-import { Card, Paragraph } from "react-native-paper";
-import styled from "styled-components/native";
 import { SvgXml } from "react-native-svg";
+import styled from "styled-components";
+import { Card } from "react-native-paper";
+
+import { Text } from "../../../components/typography/text.component";
 import star from "../../../assets/star";
 import open from "../../../assets/open";
 import { Spacer } from "../../../components/spacer/spacer.component";
-
-const Title = styled.Text`
-  font-family: ${(props) => props.theme.fonts.heading};
-  color: ${(props) => props.theme.colors.ui.primary};
-  font-size: ${(props) => props.theme.fontSizes.title};
-  font-weight: ${(props) => props.theme.fontWeights.bold};
-  line-height: ${(props) => props.theme.lineHeights.title};
-`;
-
-const RestaurantCard = styled(Card)`
-  padding: ${(props) => props.theme.space[0]};
-`;
-
-const RestaurantCardCover = styled(Card.Cover)`
-  padding: ${(props) => props.theme.space[1]};
-`;
-
-const Address = styled(Paragraph)`
-  color: ${(props) => props.theme.colors.ui.secondary};
-  font-family: ${(props) => props.theme.fonts.body};
-  font-size: ${(props) => props.theme.fontSizes.body};
-`;
-
-const Info = styled(View)`
-  padding: ${(props) => props.theme.space[2]};
-`;
-
-const Rating = styled(View)`
-  flex-direction: row;
-  padding-top: ${(props) => props.theme.space[2]};
-  padding-bottom: ${(props) => props.theme.space[2]};
-`;
-
-const Section = styled.View`
-  flex-direction: row;
-  align-items: center;
-`;
-
-const SectionEnd = styled.View`
-  flex: 1;
-  flex-direction: row;
-  justify-content: flex-end;
-`;
+import {
+  RestaurantCard,
+  RestaurantCardCover,
+  Address,
+  Info,
+  Rating,
+  Icon,
+  Section,
+  SectionEnd,
+} from "./restaurant-info-card.styles";
 
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
   const {
     name = "Pizza Palace",
-    icon,
+    icon = "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png",
     photos = [
       "https://www.foodiesfeed.com/wp-content/uploads/2022/07/pizza-with-pineapple-and-thin-crust.jpg",
     ],
@@ -70,7 +38,7 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
       <Card.Content>
         <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
         <Info>
-          <Title>{name}</Title>
+          <Text variant="label">{name}</Text>
           <Section key="1">
             <Rating>
               {ratingArray.map(() => (
@@ -80,13 +48,14 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
             <SectionEnd>
               <Spacer position="left" size="large">
                 {isClosedTemporarily && (
-                  <Text variant="label" style={{ color: "red" }}>
-                    CLOSED TEMPORARILY
-                  </Text>
+                  <Text variant="error">CLOSED TEMPORARILY</Text>
                 )}
               </Spacer>
               <Spacer position="left" size="large">
                 {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
+              </Spacer>
+              <Spacer position="left" size="large">
+                <Icon source={{ uri: icon}} />
               </Spacer>
             </SectionEnd>
           </Section>
