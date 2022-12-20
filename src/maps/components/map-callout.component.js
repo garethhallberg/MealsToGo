aff0b2a37e1414cc2cba96aa5041c7e6879ca4da
garethhallberg <gareth@ghallberg.co.uk>
2022-12-20 17:22:47 +0000

    Click marker see name and image

--- src/maps/components/map-callout.component.js
+++ src/maps/components/map-callout.component.js
@@ -1,13 +1,10 @@
 import React from "react";
 import { Text, View } from "react-native";
 import styled from "styled-components/native";
+import { CompactRestaurantInfo } from "../../components/restaurant/compact-restaurant-info.component";
 
 const SomeText = styled(Text)``;
 
 export const MapCallout = ({ restaurant }) => {
-  return (
-    <View>
-      <SomeText>{restaurant.name}</SomeText>
-    </View>
-  );
+  return <CompactRestaurantInfo restaurant={restaurant} />;
 };
